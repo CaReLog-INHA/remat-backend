27f14000665fc208383712c4cab1f62267ebe21b
zeoueon <zeoueon129@gmail.com>
2026-04-24 17:29:19 +0900

    fix: description이 null일 경우 응답에 포함되지 않도록 수정

--- src/main/java/com/remat/global/response/ResponseStatus.java
+++ src/main/java/com/remat/global/response/ResponseStatus.java
@@ -1,5 +1,6 @@
 package com.remat.global.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.remat.global.code.CommonResponseCode;
 import com.remat.global.code.ResponseCode;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseStatus {
     private String statusCode;
     private String message;
